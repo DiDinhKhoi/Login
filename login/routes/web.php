9e46fabd1da0633fec643d3e5ea699f2819464d8
DiDinhKhoi <didinhkhoi.tdc2020@gmail.com>
2024-04-12 08:59:14 +0700

    update

--- login/routes/web.php
+++ login/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', function () {
-    return view('login');
-});
+Route::get('dashboard', [UserController::class, 'dashboard']);
+Route::get('/register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class,'postUser']) -> name('user.postUser');
+Route::get('/login',[UserController::class ,'login'])->name('login');
+Route::post('check-login', [UserController::class,'CheckLogin'])->name('user.CheckLogin'); 
+Route::get('logOut', [UserController::class, 'logOut'])->name('logOut');
+Route::get('/list', [UserController::class, 'listUser']);
+Route::get('/show', [UserController::class, 'showUser'])->name('user.show');
+Route::get('/create', [UserController::class, 'create'])->name('user.create');
+Route::post('create', [UserController::class, 'createUser'])->name('user.createUser');
+Route::get('delete', [UserController::class, 'delete'])->name('user.delete');
+Route::get('/user/{id}/edit', [UserController::class, 'editUser'])->name('user.editUser');
+Route::post('/user/{id}/update', [UserController::class, 'updateUser'])->name('user.updateUser');
